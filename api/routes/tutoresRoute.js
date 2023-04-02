e88104164a707c5181b8fa56dfd5208089afc5a0
Jorge Meireles <jorgeantoniomeireles@gmail.com>
2023-04-02 14:31:50 -0300

    created tutores routes

---
+++ api/routes/tutoresRoute.js
@@ -0,0 +1,14 @@
+const {Router} = require('express');
+
+const TutoresController = require('../controllers/Tutorescontroller.js');
+
+const routerTutores = Router();
+
+routerTutores.get('/tutores', TutoresController.pegaTodosOsTutores);
+routerTutores.get('/tutor/:id', TutoresController.pegaUmTutor);
+routerTutores.post('/tutor', TutoresController.criaTutor);
+routerTutores.put('/tutor/:id', TutoresController.atualizaTutor);
+routerTutores.delete('/tutor/:id', TutoresController.apagaTutor);
+
+
+module.exports = routerTutores;
