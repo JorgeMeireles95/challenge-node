0156f2fe77ebe14729a9f2dcf40bd223ed35ea52
Jorge Meireles <jorgeantoniomeireles@gmail.com>
2023-04-02 14:32:16 -0300

    updated index

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const dotenv = require('dotenv').config();
 const express = require('express');
+const tutoresRouter = require('./api/routes/tutoresRoute.js');
 const porta = process.env.PORT || 3000;
 const app = express();
 
 
-app.get('/', (req, res) => {
-  res.send("Funciona")
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(tutoresRouter)
+
 
 app.listen(porta, () => {
 console.log(`Servidor rodando na porta ${porta}`);
