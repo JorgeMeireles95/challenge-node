f5d9cfa348358655f9a8684fb00f15c4b64d03f9
Jorge Meireles <jorgeantoniomeireles@gmail.com>
2023-04-05 00:22:28 -0300

    configuring test database

--- api/config/config.js
+++ api/config/config.js
@@ -1,10 +1,18 @@
 require('dotenv').config(); // carrega as variáveis de ambiente do arquivo .env
 
+// banco de dados desenvolvimento
 const dbUsername = process.env.DB_USERNAME;
 const dbPassword = process.env.DB_PASSWORD;
 const dbHost = process.env.DB_HOST;
 const dbName = process.env.DB_NAME;
 const dbDialect = process.env.DB_DIALECT;
+
+// banco de dados para teste
+const dbUsernameTeste = process.env.DB_USERNAME_TEST;
+const dbPasswordTeste = process.env.DB_PASSWORD_TEST;
+const dbNameTeste = process.env.DB_NAME_TEST;
+const dbHostTeste = process.env.DB_HOST_TEST;
+const dbDialectTeste = process.env.DB_DIALECT_TEST;
 
 const dbConfig = {
   development: {
@@ -15,11 +23,11 @@ const dbConfig = {
     dialect: dbDialect,
   },
   test: {
-    username: dbUsername,
-    password: dbPassword,
-    database: 'database_test',
-    host: '127.0.0.1',
-    dialect: 'mysql',
+    username: dbUsernameTeste,
+    password: dbPasswordTeste,
+    database: dbNameTeste,
+    host: dbHostTeste,
+    dialect: dbDialectTeste,
   },
   production: {
     username: dbUsername,
