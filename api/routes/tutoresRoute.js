42b445554f7ea7bc68c136840849289d37e2dfa7
Jorge Meireles <jorgeantoniomeireles@gmail.com>
2023-04-05 23:53:26 -0300

    rename the routes

--- api/routes/tutoresRoute.js
+++ api/routes/tutoresRoute.js
@@ -3,11 +3,9 @@ const { Router } = require('express');
 const TutoresController = require('../controllers/Tutorescontroller');
 
 const routerTutores = Router();
-
 routerTutores.get('/tutores', TutoresController.pegaTodosOsTutores);
-routerTutores.get('/tutor/:id', TutoresController.pegaUmTutor);
+routerTutores.get('/tutorId/:id', TutoresController.pegaUmTutor);
 routerTutores.post('/tutor', TutoresController.criaTutor);
-routerTutores.put('/tutor/:id', TutoresController.atualizaTutor);
-routerTutores.delete('/tutor/:id', TutoresController.apagaTutor);
-
+routerTutores.put('/tutorAtualizaId/:id', TutoresController.atualizaTutor);
+routerTutores.delete('/tutorExcluir/:id', TutoresController.apagaTutor);
 module.exports = routerTutores;
